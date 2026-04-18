1fe60a32b6c95199f52ef2732c8b6750f81ad6e5
Nabil Al Masri <142125053+NabilMx99@users.noreply.github.com>
2026-04-18 15:11:00 +0300

    Add vulnerable code for CodeQL analysis testing in Program.cs

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,9 +1,16 @@
-﻿
+﻿using System;
+using System.IO;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var fileName = args.Length > 0 ? args[0] : "notes.txt";
+
+        var path = Path.Combine("data", fileName);
+
+        var contents = File.ReadAllText(path);
+
+        Console.WriteLine(contents);
     }
 }
-
