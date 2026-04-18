1ed9b63cf3268570bd037cb747b85e1bc02ee607
Nabil Al Masri <142125053+NabilMx99@users.noreply.github.com>
2026-04-18 16:17:49 +0300

    Revert "Add vulnerable code for CodeQL analysis testing in Program.cs"
    This reverts commit 1fe60a32b6c95199f52ef2732c8b6750f81ad6e5.

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.IO;
-
+﻿
 class Program
 {
     static void Main(string[] args)
     {
-        var fileName = args.Length > 0 ? args[0] : "notes.txt";
-
-        var path = Path.Combine("data", fileName);
-
-        var contents = File.ReadAllText(path);
-
-        Console.WriteLine(contents);
+        Console.WriteLine("Hello, World!");
     }
 }
+
